357fcc7ad8ca259e5840a4cddaee01f098f2a183
Patrick Barbosa <patrickbarbosa@ice.ufjf.br>
2019-04-02 16:40:12 -0300

    Organizando projeto

---
+++ trabalho-ed2-cplusplus/main.cpp
@@ -0,0 +1,20 @@
+/*
+ *  Trabalho Prático de Estrutura de Dados II
+ *
+ *  Este projeto é desenvolvido para ordenar os valores
+ *  dos dados de uma API de avaliações de filmes.
+ *
+ *  Equipe de desenvolvimento:
+ *  Igor Phelipe, Mirian Mara,Patrick Barbosa, Pedro Reis
+ *  Local: Universidade Federal de Juiz de Fora
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Avaliacoes de Filmes" << endl;
+    cout << endl;
+    return 0;
+}
