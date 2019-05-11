d4ceb9bfa8b894ffacd8ea90a393c7fe42d3e0b2
igorPhelype <igor.phelype@gmail.com>
2019-05-11 17:34:25 -0300

    Cria a classe Rating

---
+++ trabalho-ed2-cplusplus/Rating.h
@@ -0,0 +1,21 @@
+//
+// Created by igorphelype on 11/05/19.
+//
+
+#ifndef TRABALHO_ED2_CPLUSPLUS_RATING_H
+#define TRABALHO_ED2_CPLUSPLUS_RATING_H
+
+
+class Rating {
+public:
+    int USERID;
+    int MOVIEID;
+    float RATING;
+    int TIMESTAMP;
+
+    Rating(int USERID, int MOVIEID, float RATING, int TIMESTAMP);
+    void print();
+};
+
+
+#endif //TRABALHO_ED2_CPLUSPLUS_RATING_H
