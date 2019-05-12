314ecc601696dba69847d48ea07e81a2a23a09ca
Pedro Reis <pedrohsareis@gmail.com>
2019-05-12 19:11:40 -0300

    Comentario pro Hygor nao errar mais

--- src/Class/QuickSort.cpp
+++ src/Class/QuickSort.cpp
@@ -4,30 +4,23 @@
 using namespace std;
 
 
-void QuickSort::sort(int v[], int low, int high){
-    
+void QuickSort::sort(int v[], int low, int high){  //Params: Array, lowest index, highest index (n-1)
     if(low < high){
         int rightPos = partition(v, low, high);
         this->sort(v, low, rightPos-1);
         this->sort(v, rightPos+1, high);
     }
-
-
 }
 
 int QuickSort::partition(int v[], int low, int high){
     int pivot = v[high];
     int i = low - 1;
     for(int j = low; j <= high-1; j++){
-
         if(v[j] <= pivot){
             i++;
             swap(v[i], v[j]);
         }
-
     }
     swap(v[i+1], v[high]);
-
     return(i+1);
-
 }
